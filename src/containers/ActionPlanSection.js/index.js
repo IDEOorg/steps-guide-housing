a3444e55e54f2c7c5ec66a4fa6bd2818362c68bf
Michael Zhong <mzhong21@gmail.com>
2017-04-25 16:52:53 -0400

    added action plan section template

---
+++ src/containers/ActionPlanSection.js/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './index.less';
+
+class ActionPlanSection extends Component {
+  render() {
+    return (
+      <div className="action_plan_section">
+        
+      </div>
+    );
+  }
+}
+
+// function mapStateToProps(state) {
+//   return {
+//   };
+// }
+//
+// function mapDispatchToProps(dispatch) {
+//   return {
+//   };
+// }
+
+export default connect(
+  {},
+  {}
+)(ActionPlanSection);
