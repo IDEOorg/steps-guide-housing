1cb815a3673a40b4b4d5b93cab1ed34936e526f4
Michael Zhong <mzhong21@gmail.com>
2017-05-02 13:57:37 -0400

    hover states

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import './index.less';
 
 export default class Card extends Component {
   render() {
-    let styles = {
-      boxShadow: '0 0 6px 1px rgba(70,70,70,0.15)'
-    };
-    let textStyle = {
-      color: 'rgba(84,133,249,0.7)'
-    };
-    if(this.props.selected) {
-      styles = {
-        boxShadow: '0 0 6px 1px rgba(84,133,249,0.4)'
-      };
-      textStyle = {
-        color: 'rgb(84,133,249)'
-      };
-    }
     return (
-      <div className="card" style={styles} onClick={() => this.props.onSelect(this.props.id)}>
-        <h2 style={textStyle}>{this.props.text}</h2>
+      <div className={classNames("card", this.props.selected ? "card_selected" : "card_unselected")} onClick={() => this.props.onSelect(this.props.id)}>
+        <h2>{this.props.text}</h2>
       </div>
     );
   }
