e7abd8fd655adacba7dafef608c1d4a7e24b700d
Michael Zhong <mzhong21@gmail.com>
2017-05-04 11:34:16 -0400

    criteria box style tweaks

--- src/components/CriteriaBox/index.js
+++ src/components/CriteriaBox/index.js
@@ -5,8 +5,8 @@ export default class CriteriaBox extends Component {
   render() {
     const criteria = this.props.criteria.map((criterion, i) => <li key={i}>{criterion}</li>);
     return (
-      <div>
-        <div className="criteria_box">
+      <div className="criteria_box">
+        <div className="criteria_section">
           <h5 className="criteria_tag">GENERAL CRITERIA</h5>
           <ul className="criteria_list">{criteria}</ul>
         </div>
