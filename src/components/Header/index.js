35557ec3dd750aa88718abcf0b96422e18271aae
Michael Zhong <mzhong21@gmail.com>
2017-05-04 11:44:54 -0400

    header icon and charity icon from jpeg to png

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,8 @@ import './index.less';
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <div className="header"></div>
+      <div className="header">
+        <img src={require('../../assets/header-icon.svg')} />
       </div>
     );
   }
