7b4a7b914518c2ba67f425a752da43fe9297d95f
Michael Zhong <mzhong21@gmail.com>
2017-05-23 18:47:35 -0400

    changed routing to hashhistory

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import initialState from './store/initialState';
 import configureStore from './store/configureStore';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import Routes from './routes';
 import '../vendors/sanitize.min.css';
 import './index.less';
 
 const store = configureStore(initialState);
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(routerMiddleware(browserHistory))
+    applyMiddleware(routerMiddleware(hashHistory))
   );
 }
